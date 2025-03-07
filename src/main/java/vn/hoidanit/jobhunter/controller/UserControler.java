ccb5fd621271636400e20660de6494fdca349d22
SOS Nu <144415137+SOS-Nu@users.noreply.github.com>
2025-03-07 14:20:10 +0700

    @pathvariable delete user

--- src/main/java/vn/hoidanit/jobhunter/controller/UserControler.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserControler.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -17,11 +19,17 @@ public class UserControler {
         this.userService = userService;
     }
 
-    @PostMapping("/user/create")
+    @PostMapping("/user")
     public User createNewUser(@RequestBody User userPostMan) {
 
         User nuUser = this.userService.handleCreateUser(userPostMan);
 
         return nuUser;
+    }
+
+    @DeleteMapping("user/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        this.userService.handleDeleteUser(id);
+        return "nuUser";
     }
 }
