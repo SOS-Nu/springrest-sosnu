07914c2db0cb530ba523d1708b6db290d9c722d7
SOS Nu <144415137+SOS-Nu@users.noreply.github.com>
2025-03-17 12:57:56 +0700

    update http statuscode , body cho api

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Java Spring Starter template
-Template này được sử dụng cho series Java Spring Restful (dự án jobHunter) của tác giả Hỏi Dân IT (Eric)
-
+Template này được sử dụng cho series Java Spring Restful (dự án jobHunter) của tác giả Lê Văn Nguyên (SOS Nu)
 ## Về tác giả
-Mọi thông tin về Tác giả Hỏi Dân IT, các bạn có thể tìm kiếm tại đây:
+Mọi thông tin về Tác giả SOS Nu, các bạn có thể tìm kiếm tại đây:
 
---
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -0,0 +1,61 @@
+package vn.hoidanit.jobhunter.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.jobhunter.domain.User;
+import vn.hoidanit.jobhunter.service.UserService;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createNewUser(@RequestBody User userPostMan) {
+        User nuUser = this.userService.handleCreateUser(userPostMan);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuUser);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") long id) {
+        this.userService.handleDeleteUser(id);
+        return ResponseEntity.ok("nuUser");
+    }
+
+    // fetch user by id
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> fetchUserById(@PathVariable("id") long id) {
+        User fetchUser = this.userService.fetchUserById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(fetchUser);
+
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+
+        return ResponseEntity.status(HttpStatus.OK).body(this.userService.fetchAllUser());
+
+    }
+
+    @PutMapping("/users")
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        User nuUser = this.userService.handleUpdateUser(user);
+        return ResponseEntity.ok(nuUser);
+    }
+
+}
