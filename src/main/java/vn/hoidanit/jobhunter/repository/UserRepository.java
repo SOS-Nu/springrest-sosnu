98ac8d2d52d4290406d527003817bd5b399006c5
SOS Nu <144415137+SOS-Nu@users.noreply.github.com>
2025-03-27 17:33:46 +0700

    query filter operator like

--- src/main/java/vn/hoidanit/jobhunter/repository/UserRepository.java
+++ src/main/java/vn/hoidanit/jobhunter/repository/UserRepository.java
@@ -3,12 +3,13 @@ package vn.hoidanit.jobhunter.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import vn.hoidanit.jobhunter.domain.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
     User findByEmail(String Email);
 
 }
