9718e46d502c18b776747ed41e45052ec385e5bb
nhokanhanh@gmail.com <nhokanhanh@gmail.com>
2025-12-05 19:51:38 +0700

    file excel

--- src/main/java/vn/hoidanit/jobhunter/controller/GeminiController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/GeminiController.java
@@ -1,9 +1,16 @@
 package vn.hoidanit.jobhunter.controller;
 
+import java.io.IOException;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
 import vn.hoidanit.jobhunter.domain.response.ResFindCandidatesDTO;
 import vn.hoidanit.jobhunter.domain.response.ResInitiateCandidateSearchDTO;
 import vn.hoidanit.jobhunter.domain.response.ai.ResCvEvaluationDTO;
@@ -13,8 +20,6 @@ import vn.hoidanit.jobhunter.service.FileService;
 import vn.hoidanit.jobhunter.service.GeminiService;
 import vn.hoidanit.jobhunter.util.annotation.ApiMessage;
 import vn.hoidanit.jobhunter.util.error.IdInvalidException;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/gemini")
